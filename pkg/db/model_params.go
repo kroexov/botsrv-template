cd88d7642e0a0de1fba5bcee8eed57e03b2b1228
kroexov <kroexov@gmail.com>
2025-11-11 03:07:22 +0300

    Add UserTimeSlots definition

--- pkg/db/model_params.go
+++ pkg/db/model_params.go
@@ -1,4 +1,16 @@
 package db
 
 type UserTimeSlots struct {
+	WeekDays []WeekDay `json:"weekDays"`
+}
+
+// WeekDay represents day of the week from 1 (Monday) to 7 (Sunday) with array of TimeSlot
+type WeekDay struct {
+	DayNumber int        `json:"dayNumber"`
+	Slots     []TimeSlot `json:"slots"`
+}
+
+// TimeSlot has id of 1..16, each representing 1 hour slot from 6:00 am to 10:00 pm
+type TimeSlot struct {
+	Id int `json:"id"`
 }
